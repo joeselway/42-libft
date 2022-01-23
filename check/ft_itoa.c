dcc834d28d56c054de5369478ffd11248b1c58ce
Joe Selway <17176434+joeselway@users.noreply.github.com>
2022-01-24 10:27:20 +1030

    ft_itoa done not tested

---
+++ check/ft_itoa.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jselway <jselway@student.42adel.org.au>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 14:08:06 by jselway           #+#    #+#             */
+/*   Updated: 2022/01/24 10:24:41 by jselway          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_base10digits(long int n)
+{
+	int	i;
+
+	if (n == 0)
+		return (0);
+	i = 1;
+	while (n > 0)
+	{
+		i++;
+		n = n / 10;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	long int	nb;
+	int			negflag;
+	int			len;
+	int			i;
+	char		*str;
+
+	nb = n;
+	negflag = 0;
+	if (nb < 0)
+	{
+		sign *= -1;
+		nb *= -1;
+		len = ft_base10digits(nb) + 1;
+	}
+	else
+		len = ft_base10digits(nb);
+	str = malloc(sizeof(*str) * len);
+	if (negflag == 1)
+		str[0] = '-';
+	while ((len - 1) >= (0 + negflag))
+	{
+		str[len] = nb % 10;
+		nb = nb / 10;
+	}
+	return (str);
+}
