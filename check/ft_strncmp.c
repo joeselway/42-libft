1f3ba3d94123f43a501d87b73d1cdcf7d40f46d5
Joe Selway <17176434+joeselway@users.noreply.github.com>
2022-02-02 19:11:20 +1030

    fix strncmp

--- check/ft_strncmp.c
+++ check/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jselway <jselway@student.42adel.org.au>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 17:23:44 by jselway           #+#    #+#             */
-/*   Updated: 2022/02/01 11:34:04 by jselway          ###   ########.fr       */
+/*   Updated: 2022/02/02 19:10:51 by jselway          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
-	{
-		if (s1[i] == s2[i])
-			i++;
-	}
-	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
+	while (s1[i] && s1[i] == s2[i] && i < n)
+		i++;
+	if (i < n)
+		return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
+	return (0);
 }
