55e1ea5815e671979cabe028e5574352cb115ecf
Joe Selway <17176434+joeselway@users.noreply.github.com>
2022-05-15 21:14:29 +0930

    fix strlcat edge returns

--- check/ft_strlcat.c
+++ check/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jselway <jselway@student.42adel.org.au>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 17:56:40 by jselway           #+#    #+#             */
-/*   Updated: 2022/02/02 19:18:07 by jselway          ###   ########.fr       */
+/*   Updated: 2022/05/15 21:13:49 by jselway          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 
 	dstlen = ft_strlen(dst);
 	if (dstsize == 0)
-		return (dstlen + ft_strlen(src));
+		return (ft_strlen(src));
 	i = 0;
-	while (src[i] && i < (dstsize - dstlen - 1))
+	while (src[i] && (i + dstlen) < (dstsize - 1))
 	{
 		dst[dstlen + i] = src[i];
 		i++;
 	}
-	if (dstsize && dstsize < dstlen)
+	if (dstlen + i < dstsize)
 		dst[dstlen + i] = '\0';
 	return (dstlen + ft_strlen(src));
 }	
