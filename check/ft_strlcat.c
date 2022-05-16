6253578def85ac07829120982ea7f60170475845
Joe Selway <17176434+joeselway@users.noreply.github.com>
2022-05-16 20:03:54 +0930

    ft_strlcat: fix stupid strlen error

--- check/ft_strlcat.c
+++ check/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jselway <jselway@student.42adel.org.au>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 17:56:40 by jselway           #+#    #+#             */
-/*   Updated: 2022/05/15 21:13:49 by jselway          ###   ########.fr       */
+/*   Updated: 2022/05/16 20:03:04 by jselway          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t	i;
 	size_t	dstlen;
+	size_t	srclen;
 
 	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
 	if (dstsize == 0)
-		return (ft_strlen(src));
+		return (srclen);
 	i = 0;
 	while (src[i] && (i + dstlen) < (dstsize - 1))
 	{
@@ -28,5 +30,5 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 	}
 	if (dstlen + i < dstsize)
 		dst[dstlen + i] = '\0';
-	return (dstlen + ft_strlen(src));
+	return (dstlen + srclen);
 }	
